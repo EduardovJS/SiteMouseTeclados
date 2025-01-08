3925ab03575e8102ce862554ff76aa6973fe22e4
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-01-08 20:06:56 -0300

    Models definidas

--- SiteMouseTeclados/Context/AppDbContext.cs
+++ SiteMouseTeclados/Context/AppDbContext.cs
@@ -6,8 +6,6 @@ namespace SiteMouseTeclados.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
         DbSet<Produto> Produtos { get; set; }   
-
     }
 }
