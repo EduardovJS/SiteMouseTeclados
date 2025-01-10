284c1d75018c86171815e1c1484884c2567ad011
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-01-09 22:46:53 -0300

    Fazendo a tela de criar produto

--- SiteMouseTeclados/Controllers/ProdutosController.cs
+++ SiteMouseTeclados/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SiteMouseTeclados.Context;
+using SiteMouseTeclados.Models;
 using SiteMouseTeclados.ViewModels;
 
 public class ProdutosController : Controller
@@ -15,7 +16,7 @@ public class ProdutosController : Controller
     {
         var produtos = _context.Produtos.ToList();
 
-        var produtoViewModels = produtos.Select(p=> new ProdutoViewModel
+        var produtoViewModels = produtos.Select(p => new ProdutoViewModel
         {
             ProdutoId = p.Id,
             Nome = p.Nome,
@@ -26,6 +27,40 @@ public class ProdutosController : Controller
         }).ToList();
 
         return View(produtoViewModels);
-
     }
+
+    public IActionResult NovoProduto()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> NovoProduto(ProdutoViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Mapear o ViewModel para a entidade Produto
+            var produto = new Produto
+            {
+                Nome = model.Nome,
+                Resumo = model.Resumo,
+                Categoria = model.Categoria,
+                Valor = model.Valor,
+                ImageFileName = model.ImageFileName // Salvar o nome do arquivo como texto
+            };
+
+            // Salvar no banco de dados
+            _context.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(model);
+    }
+
+
+
+
+
+
 }
